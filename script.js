620002869613ae452a361ea5ba1eb42362a395f6
Deo Seth Jabagat <diothewind@gmail.com>
2023-02-25 17:13:02 +0800

    Add getComputerChoice function that will return either rock, paper, or scissors from the computer

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("test")
+function getComputerChoice(){
+    const computerChoice = ["rock", "paper", "scissors"];
+    return computerChoice[Math.floor(Math.random() * 3)];
+}   
+
+console.log(getComputerChoice())
