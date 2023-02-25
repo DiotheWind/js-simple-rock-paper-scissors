d87b7e3e1fe98a18ecb32192d920a59fe71e4d01
Deo Seth Jabagat <diothewind@gmail.com>
2023-02-25 17:35:12 +0800

    Add game function to check who wins

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// function game(playerSelection, computerSelection){
+function game(playerSelection, computerSelection){
+    if((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")){
+        console.log(`It's a tie because the computer also chose ${computerSelection}`)
+    }else if(playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You lose! Computer chose paper")
+    }else if(playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("You win! Computer chose scissors")
+    }else if(playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You win! Computer chose rock")
+    }else if(playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("You lose! Computer chose scissors")
+    }else if(playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You lose! Computer chose rock")
+    }else if(playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("You win! Computer chose paper")
+    }else{
+        console.log("Invalid")
+    }
+} 
 
-// }
-
+function getPlayerChoice(){
+    let computerChoice = prompt("Enter your pick here");
+    return computerChoice.toLowerCase().trim();   
+}
 
 function getComputerChoice(){
     const computerChoice = ["rock", "paper", "scissors"];
@@ -9,10 +29,6 @@ function getComputerChoice(){
 }   
 
 
-function getPlayerChoice(){
-    let computerChoice = prompt("Enter your pick here")
-    return computerChoice.toLowerCase().trim()   
-}
 
-let test = getPlayerChoice();
-console.log(test);
+
+game(getPlayerChoice(), getComputerChoice());
