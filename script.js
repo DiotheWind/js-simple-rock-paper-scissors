01480fea1ed97758b00bd523d6c654157434cb84
Deo Seth Jabagat <diothewind@gmail.com>
2023-02-25 17:40:06 +0800

    Add game function that let user play five rounds and add a score system to see who won at the end of five rounds

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+let playerScore;
+let computerScore;
+
+function game(){
+    for(let i = 1; i <= 5; i++){
+        playRound(getPlayerChoice(), getComputerChoice());
+    }
+}
+
 function playRound(playerSelection, computerSelection){
     if((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")){
         console.log(`It's a tie because the computer also chose ${computerSelection}`)
     }else if(playerSelection === "rock" && computerSelection === "paper"){
         console.log("You lose! Computer chose paper");
+        computerScore++;
     }else if(playerSelection === "rock" && computerSelection === "scissors"){
         console.log("You win! Computer chose scissors");
+        playerScore++;
     }else if(playerSelection === "paper" && computerSelection === "rock"){
         console.log("You win! Computer chose rock");
+        playerScore++;
     }else if(playerSelection === "paper" && computerSelection === "scissors"){
         console.log("You lose! Computer chose scissors");
+        computerScore++;
     }else if(playerSelection === "scissors" && computerSelection === "rock"){
         console.log("You lose! Computer chose rock");
+        computerScore++;
     }else if(playerSelection === "scissors" && computerSelection === "paper"){
         console.log("You win! Computer chose paper");
+        playerScore++;
     }else{
         console.log("Invalid");
     }
